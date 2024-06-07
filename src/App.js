29a38c1992b9746c6af6e2223abf622947321639
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-06-07 15:38:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const[post,setPost] = useState([]);
+
+  const[startdate,setStartdate] = useState();
+  const[enddate,setEnddate] = useState();
+  const[cost,setCost] = useState();
+  const[description,setDescription] = useState();
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/posts")
+    .then((posts) => setPost(posts.data))
+    .catch((err) => console.log(err));
+  },[post]);
+
+  const submit = () => {
+    axios.post("http://localhost:5000/posts",{startdate,enddate,cost,description})
+  }
+  return(
+    <>
+
+      {
+        post.map((postt) => {
+          return(
+            <>
+              <tr>
+                <td>{postt.startdate}</td>
+                <td>{postt.enddate}</td>
+                <td>{postt.cost}</td>
+                <td>{postt.description}</td>
+              </tr>
+              {/* <h1>Start Date:{postt.startdate}</h1>
+              <h1>End Date:{postt.enddate}</h1>
+              <h1>Overal Cost :{postt.cost}</h1>
+              <h1>Description: {postt.description}</h1> */}
+            </>
+          )
+        })
+      }
+      <input type='text' placeholder='Start Date' onChange={(e) => setStartdate(e.target.value)}></input>
+      <input type='text' placeholder='End Date' onChange={(e) => setEnddate(e.target.value)}></input>
+      <input type='text' placeholder='Cost' onChange={(e) => setCost(e.target.value)}></input>
+      <input type='text' placeholder='Description' onChange={(e) => setDescription(e.target.value)}></input>
+      <button onClick={submit}>Submit</button>
+
+    </>
+  )
 }
 
 export default App;
